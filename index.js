23e4f53b900dff5e674a243224db1b2f43dc797c
Sasaya <storyn26383@gmail.com>
2020-10-22 01:06:30 +0800

    support alpha variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+const createColor = require('color')
 const plugin = require('tailwindcss/plugin')
 const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default
 
@@ -8,7 +10,62 @@ const sides = {
   l: 'left'
 }
 
-module.exports = plugin(({ theme, e, addUtilities, variants }) => {
+const hasAlpha = (color) => {
+  return (
+    color.startsWith('rgba(') ||
+    color.startsWith('hsla(') ||
+    (color.startsWith('#') && color.length === 9) ||
+    (color.startsWith('#') && color.length === 5)
+  )
+}
+
+
+const toRgba = (color) => {
+  const [r, g, b, a] = createColor(color).rgb().array()
+
+  return [r, g, b, a === undefined && hasAlpha(color) ? 1 : a]
+}
+
+const withAlphaVariable = ({ color, property, variable }) => {
+  if (_.isFunction(color)) {
+    return {
+      [variable]: '1',
+      [property]: color({ opacityVariable: variable })
+    }
+  }
+
+  try {
+    const [r, g, b, a] = toRgba(color)
+
+    if (a !== undefined) {
+      return { [property]: color }
+    }
+
+    return {
+      [variable]: '1',
+      [property]: `rgba(${r}, ${g}, ${b}, var(${variable}))`,
+    }
+  } catch (error) {
+    return { [property]: color }
+  }
+}
+
+const toColorValue = (maybeFunction) => {
+  return _.isFunction(maybeFunction) ? maybeFunction({}) : maybeFunction
+}
+
+module.exports = plugin(({ theme, e, addUtilities, variants, corePlugins }) => {
+  const getProperties = ({ side, color }) => {
+    const property = `border-${side}-color`
+    const variable = '--border-opacity'
+
+    if (corePlugins('borderOpacity')) {
+      return withAlphaVariable({ color, property, variable })
+    }
+
+    return { [property]: toColorValue(color) }
+  }
+
   const colors = flattenColorPalette(theme('borderColor'))
   const utilities = {}
 
@@ -17,13 +74,12 @@ module.exports = plugin(({ theme, e, addUtilities, variants }) => {
       continue
     }
 
+
     for (const sideModifier in sides) {
       const side = sides[sideModifier]
       const color = colors[colorModifier]
 
-      utilities[`.${e(`border-${sideModifier}-${colorModifier}`)}`] = {
-        [`border-${side}-color`]: color
-      }
+      utilities[`.${e(`border-${sideModifier}-${colorModifier}`)}`] = getProperties({ side, color })
     }
   }
 
